824c79e38f9135363a64374c2ea17619249f0bf3
pengrobin <pengrobin@gmail.com>
2020-04-14 22:26:35 +0800

    第八周课件及示例代码-2

---
+++ L07QARiverStarter/src/test/java/cn/edu/zucc/pb/qa/QaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.zucc.pb.qa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ L08QARiver/src/main/java/cn/edu/zucc/pb/qa/m8/L08QaRiverApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.zucc.pb.qa.m8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L08QaRiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L08QaRiverApplication.class, args);
+    }
+
+}
